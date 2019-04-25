92906079a8db18ad027e44bb6bc9e50b8003ff0a
Rupesh <sawaliya.rupesh@gmail.com>
2019-04-25 14:45:48 +0530

    Implemented scenario for unknown numbers of input.

--- src/main/java/com/tdd/utility/StringCalculator.java
+++ src/main/java/com/tdd/utility/StringCalculator.java
@@ -23,10 +23,22 @@ public class StringCalculator {
 			sum = 0;
 		} else if (splitNumberString.length == 1) {
 			sum = Integer.parseInt(numberString);
-		}else {
-			sum=Integer.parseInt(splitNumberString[0]) + Integer.parseInt(splitNumberString[1]);
+		} else {
+			sum = getSum(splitNumberString);
 		}
 		return sum;
 	}
-
+	
+	/**
+	 * This method is responsible for adding supplied numbers in string array.
+	 * @param numbersString - array of number string
+	 * @return sum
+	 */
+	private int getSum(String[] numbersString) {
+		int sum = 0;
+		for (String numberString : numbersString) {
+			sum += Integer.parseInt(numberString);
+		}
+		return sum;
+	}
 }
